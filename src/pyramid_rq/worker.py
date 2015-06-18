6452cddc1b7347ed6c0659722705242d63bdd3c4
Piotr Jarolewski <piotr.jarolewski@allegrogroup.com>
2015-06-18 09:26:50 +0200

    Add logger to prqworker

--- src/pyramid_rq/worker.py
+++ src/pyramid_rq/worker.py
@@ -6,7 +6,7 @@ try:
     HAVE_TRANSACTION = True
 except ImportError:
     HAVE_TRANSACTION = False
-from pyramid.paster import bootstrap
+from pyramid.paster import bootstrap, setup_logging
 from rq import Queue, Worker
 from redis.exceptions import ConnectionError
 
@@ -17,8 +17,11 @@ class PyramidWorker(Worker):
         self.environment = environment
 
     def perform_job(self, job):
-        self.procline('Initializing pyramid for %s from %s' %
-                (job.func_name, job.origin))
+        self.procline(
+            'Initializing pyramid for %s from %s' % (
+                job.func_name, job.origin
+            )
+        )
         try:
             super(PyramidWorker, self).perform_job(job)
         except:
@@ -33,11 +36,12 @@ class PyramidWorker(Worker):
 
 
 def run(options):
+    setup_logging(options.config)
     environment = bootstrap(options.config)
     settings = environment['registry'].settings
     if 'rq.redis' not in settings:
         print >> sys.stderr, 'Critical error: ' \
-                'pyramid_rq not configured by application'
+            'pyramid_rq not configured by application'
         sys.exit(1)
     with rq.Connection(environment['registry'].settings['rq.redis']):
         try:
@@ -50,18 +54,37 @@ def run(options):
 
 def main():  # pragma: no coverage
     parser = argparse.ArgumentParser(
-            description='Perform regular cleanup tasks')
-    parser.add_argument('config', metavar='<ini-file>',
-            help='Configuration file (and optionally section)')
-    parser.add_argument('--burst', '-b', action='store_true',
-            default=False,
-            help='Run in burst mode (quit after all work is done)')
-    parser.add_argument('--name', '-n', default=None,
-            help='Specify a different name')
-    parser.add_argument('--verbose', '-v', action='store_true',
-            default=False, help='Show more output')
-    parser.add_argument('queues', nargs='*', default=['default'],
-            help='The queues to listen on (default: \'default\')')
+        description='Perform regular cleanup tasks')
+    parser.add_argument(
+        'config',
+        metavar='<ini-file>',
+        help='Configuration file (and optionally section)'
+    )
+    parser.add_argument(
+        '--burst', '-b',
+        action='store_true',
+        default=False,
+        help='Run in burst mode (quit after all work is done)'
+    )
+    parser.add_argument(
+        '--name',
+        '-n',
+        default=None,
+        help='Specify a different name'
+    )
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        action='store_true',
+        default=False,
+        help='Show more output'
+    )
+    parser.add_argument(
+        'queues',
+        nargs='*',
+        default=['default'],
+        help='The queues to listen on (default: \'default\')'
+    )
     options = parser.parse_args()
     run(options)
 
